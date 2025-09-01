71cbee934f8610a747eae2b34d90bbc78aa95c10
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-09-01 18:29:57 +0200

    Some PRF fitting tweaks

--- retsupp/modeling/fit_prf.py
+++ retsupp/modeling/fit_prf.py
@@ -7,8 +7,9 @@ from braincoder.models import GaussianPRF2DWithHRF, DifferenceOfGaussiansPRF2DWi
 from braincoder.optimize import ParameterFitter
 from braincoder.hrf import SPMHRFModel
 import numpy as np
+import pandas as pd
 
-def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05, max_n_iterations=100, debug=False, resolution=50):
+def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05, max_n_iterations=2000, debug=False, resolution=50):
 
     print(f"Fitting model: {model_label}")
 
@@ -45,12 +46,12 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
 
     # Load the PRF model
 
-    hrf_model = SPMHRFModel(tr=1.6, delay=4.5, dispersion=.75)
+    hrf_model = SPMHRFModel(tr=1.6, delay=4.5, dispersion=0.75)
     prf_model = GaussianPRF2DWithHRF(grid_coordinates, paradigm, hrf_model=hrf_model)
 
     grid_x = np.linspace(-3, 3, 12)
     grid_y = np.linspace(-3, 3, 12)
-    grid_sd = np.linspace(.33, 3, 8)
+    grid_sd = np.linspace(1., 4., 8)
     # grid_x = np.linspace(-4, 4, 3)
     # grid_y = np.linspace(-4, 4, 3)
     # grid_sd = np.linspace(.33, 6, 3)
@@ -60,13 +61,16 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
     # Grid fit
     fitter = ParameterFitter(prf_model, data, paradigm,)
     grid_pars = fitter.fit_grid(grid_x, grid_y, grid_sd, grid_baseline, grid_amplitude, use_correlation_cost=True)
-    grid_pars = fitter.refine_baseline_and_amplitude(grid_pars)
+
+    grid_pars = fitter.refine_baseline_and_amplitude(grid_pars, l2_alpha=0.001)
+
     grid_pars['theta'] = np.arctan2(grid_pars['y'], grid_pars['x'])
     grid_pars['ecc'] = np.sqrt(grid_pars['x']**2 + grid_pars['y']**2)
     grid_pars['r2'] = fitter.get_rsq(grid_pars)
     r2_grid = grid_pars['r2'].values
 
-    print(r2_grid)
+    print(pd.Series(r2_grid).describe())
+    print(f'Percentage of selected voxels (>r2_thr): {(r2_grid > grid_r2_thr).sum()} / {len(r2_grid)} ({(r2_grid > grid_r2_thr).mean() * 100:.2f}%)')
 
     r2_mask = image.math_img(f'r2 > {grid_r2_thr}', r2=brain_masker.inverse_transform(grid_pars['r2']))
 
@@ -75,7 +79,7 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
     data = r2_masker.fit_transform(mean_ts)
     grid_pars_thr = grid_pars[r2_grid > grid_r2_thr]
     fitter = ParameterFitter(prf_model, data, paradigm)
-    gd_pars = fitter.fit(init_pars=grid_pars_thr, max_n_iterations=max_n_iterations, learning_rate=0.001,)
+    gd_pars = fitter.fit(init_pars=grid_pars_thr, max_n_iterations=max_n_iterations, learning_rate=0.005,)
     pred = fitter.predictions
     r2 = fitter.get_rsq(gd_pars)
 
@@ -101,7 +105,7 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
         par_fitter_dog = ParameterFitter(model=model_dog, data=data, paradigm=paradigm)
 
         # Note how, for now, we are not optimizing the HRF parameters.
-        pars_dog = par_fitter_dog.fit(init_pars=pars_dog_init, max_n_iterations=max_n_iterations, learning_rate=0.001,)
+        pars_dog = par_fitter_dog.fit(init_pars=pars_dog_init, max_n_iterations=max_n_iterations, learning_rate=0.005,)
 
         pars_dog['theta'] = np.arctan2(pars_dog['y'], pars_dog['x'])
         pars_dog['ecc'] = np.sqrt(pars_dog['x']**2 + pars_dog['y']**2)
@@ -109,7 +113,7 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
 
         final_pars = pars_dog.copy()
         pred = par_fitter_dog.predictions
-        r2 = fitter.get_rsq(final_pars)
+        r2 = par_fitter_dog.get_rsq(final_pars)
 
 
     elif model_label == 3:
@@ -122,7 +126,7 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
 
         gaussian_hrf_fitter = ParameterFitter(model=gaussian_hrf_model, data=data, paradigm=paradigm)
 
-        gaussian_hrf_pars = gaussian_hrf_fitter.fit(init_pars=gaussian_hrf_init_pars, max_n_iterations=max_n_iterations)
+        gaussian_hrf_pars = gaussian_hrf_fitter.fit(init_pars=gaussian_hrf_init_pars, max_n_iterations=max_n_iterations, learning_rate=0.005)
 
         r2 = gaussian_hrf_fitter.get_rsq(gaussian_hrf_pars)
         pred = gaussian_hrf_fitter.predictions
@@ -143,7 +147,7 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
 
         dog_hrf_fitter = ParameterFitter(model=dog_hrf_model, data=data, paradigm=paradigm)
 
-        dog_hrf_pars = dog_hrf_fitter.fit(init_pars=dog_hrf_init_pars, max_n_iterations=max_n_iterations)
+        dog_hrf_pars = dog_hrf_fitter.fit(init_pars=dog_hrf_init_pars, max_n_iterations=max_n_iterations, learning_rate=0.005)
 
         r2 = dog_hrf_fitter.get_rsq(dog_hrf_pars)
 
@@ -175,16 +179,22 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05,
         par_img.to_filename(target_dir / f'sub-{subject:02d}_desc-{par}.nii.gz')
 
 
+    pred_img = r2_masker.inverse_transform(pred)
+    pred_img.to_filename(target_dir / f'sub-{subject:02d}_desc-pred.nii.gz')
+
 if __name__ == "__main__":
     # Parse command line arguments
     argument_parser = argparse.ArgumentParser(description="Fit PRF model")
     argument_parser.add_argument('subject', type=int, help='Subject ID')
     argument_parser.add_argument('--model', type=int, default=1)
     argument_parser.add_argument('--r2_thr', default=0.04, type=float, help='R2 threshold for grid fit')
-    argument_parser.add_argument('--resolution', default=50, type=int, help='Resolution of PRF stimulus')
+    argument_parser.add_argument('--resolution', default=40, type=int, help='Resolution of PRF stimulus')
+    argument_parser.add_argument('--max_n_iterations', default=4000, type=int, help='Number of iterations for each optimisation step')
+
     argument_parser.add_argument('--bids_folder', default='/data/ds-retsupp', help='BIDS folder path')
     argument_parser.add_argument('--debug', action='store_true', help='Enable debug mode')  
 
     args = argument_parser.parse_args()
 
-    main(args.subject, model_label=args.model, bids_folder=args.bids_folder, grid_r2_thr=args.r2_thr, debug=args.debug, resolution=args.resolution)
+    main(args.subject, model_label=args.model, bids_folder=args.bids_folder, grid_r2_thr=args.r2_thr, 
+         debug=args.debug, resolution=args.resolution, max_n_iterations=args.max_n_iterations)
