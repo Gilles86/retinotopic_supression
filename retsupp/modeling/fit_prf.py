bf0af4ef6614c78023dc25d8a129b69f05e3c941
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-09-01 15:25:14 +0200

    Newer PRF models, some cleanup...

--- retsupp/modeling/fit_prf.py
+++ retsupp/modeling/fit_prf.py
@@ -8,32 +8,44 @@ from braincoder.optimize import ParameterFitter
 from braincoder.hrf import SPMHRFModel
 import numpy as np
 
-def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.04, max_n_iterations=5000):
+def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.05, max_n_iterations=100, debug=False, resolution=50):
 
     print(f"Fitting model: {model_label}")
 
     bids_folder = Path(bids_folder)
-    target_dir = bids_folder / 'derivatives' / 'prf' / f'model{model_label}' / f'sub-{subject:02d}'
+    if debug:
+        target_dir = bids_folder / 'derivatives' / 'prf.debug' / f'model{model_label}' / f'sub-{subject:02d}'
+    else:
+        target_dir = bids_folder / 'derivatives' / 'prf' / f'model{model_label}' / f'sub-{subject:02d}'
+
     target_dir.mkdir(parents=True, exist_ok=True)
 
     sub = Subject(subject, bids_folder)
 
     bold_mask = sub.get_bold_mask()
+
+    if debug:
+        bold_mask = image.math_img('np.where(mask.astype(bool) & (np.random.rand(*mask.shape) < 0.01), 1, 0)', mask=bold_mask)
+        max_n_iterations = 100
+
     mean_ts = bids_folder / 'derivatives' / 'mean_signal' / f'sub-{subject:02d}'/ f'sub-{subject:02d}_desc-mean_bold.nii.gz'
 
     brain_masker = input_data.NiftiMasker(mask_img=bold_mask)
 
     data = brain_masker.fit_transform(mean_ts)
 
-    paradigm = sub.get_stimulus(resolution=50).astype(np.float32)
-    grid_coordinates = sub.get_grid_coordinates(resolution=50)
+    if debug:
+        resolution = 20
+
+    paradigm = sub.get_stimulus(resolution=resolution).astype(np.float32)
+    grid_coordinates = sub.get_grid_coordinates(resolution=resolution)
     grid_coordinates = np.stack((grid_coordinates[0].ravel(), grid_coordinates[1].ravel()), axis=1).astype(np.float32)
     paradigm = paradigm.reshape((paradigm.shape[0], -1))
 
 
     # Load the PRF model
 
-    hrf_model = SPMHRFModel(tr=1.6)
+    hrf_model = SPMHRFModel(tr=1.6, delay=4.5, dispersion=.75)
     prf_model = GaussianPRF2DWithHRF(grid_coordinates, paradigm, hrf_model=hrf_model)
 
     grid_x = np.linspace(-3, 3, 12)
@@ -54,6 +66,8 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.04,
     grid_pars['r2'] = fitter.get_rsq(grid_pars)
     r2_grid = grid_pars['r2'].values
 
+    print(r2_grid)
+
     r2_mask = image.math_img(f'r2 > {grid_r2_thr}', r2=brain_masker.inverse_transform(grid_pars['r2']))
 
     # GD fit
@@ -62,24 +76,14 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.04,
     grid_pars_thr = grid_pars[r2_grid > grid_r2_thr]
     fitter = ParameterFitter(prf_model, data, paradigm)
     gd_pars = fitter.fit(init_pars=grid_pars_thr, max_n_iterations=max_n_iterations, learning_rate=0.001,)
+    pred = fitter.predictions
+    r2 = fitter.get_rsq(gd_pars)
 
     if model_label == 1:
-        gd_pars['theta'] = np.arctan2(gd_pars['y'], gd_pars['x'])
-        gd_pars['ecc'] = np.sqrt(gd_pars['x']**2 + gd_pars['y']**2)
-        gd_pars['r2'] = fitter.get_rsq(gd_pars)
+        final_pars = gd_pars.copy()
 
-        # save the results
-        for par in gd_pars.columns:
-            gd_par_img = r2_masker.inverse_transform(gd_pars[par])
-            grid_par_img = brain_masker.inverse_transform(grid_pars[par])
-            r2_grid_img = brain_masker.inverse_transform(r2_grid)
 
-            par_img = image.math_img(f'np.where(r2 > {grid_r2_thr}, gd_pars, grid_pars)', 
-                                    r2=r2_grid_img, gd_pars=gd_par_img, grid_pars=grid_par_img)
-
-            par_img.to_filename(target_dir / f'sub-{subject:02d}_desc-{par}.nii.gz')
-
-    elif model_label == 2:
+    elif model_label == 2: # DOG
         print('yo')
         model_dog = DifferenceOfGaussiansPRF2DWithHRF(data=data, paradigm=paradigm, hrf_model=hrf_model,
                                                 grid_coordinates=grid_coordinates, flexible_hrf_parameters=False)
@@ -103,16 +107,72 @@ def main(subject, model_label, bids_folder='/data/ds-retsupp', grid_r2_thr=0.04,
         pars_dog['ecc'] = np.sqrt(pars_dog['x']**2 + pars_dog['y']**2)
         pars_dog['r2'] = fitter.get_rsq(pars_dog)
 
-        for par in pars_dog.columns:
-            pars_dog_img = r2_masker.inverse_transform(pars_dog[par])
+        final_pars = pars_dog.copy()
+        pred = par_fitter_dog.predictions
+        r2 = fitter.get_rsq(final_pars)
+
+
+    elif model_label == 3:
+        gaussian_hrf_model = GaussianPRF2DWithHRF(grid_coordinates, paradigm, hrf_model=hrf_model, flexible_hrf_parameters=True)
+
+        gaussian_hrf_init_pars = gd_pars.copy() 
+
+        gaussian_hrf_init_pars['hrf_delay'] = 4.5
+        gaussian_hrf_init_pars['hrf_dispersion'] = .75
+
+        gaussian_hrf_fitter = ParameterFitter(model=gaussian_hrf_model, data=data, paradigm=paradigm)
+
+        gaussian_hrf_pars = gaussian_hrf_fitter.fit(init_pars=gaussian_hrf_init_pars, max_n_iterations=max_n_iterations)
+
+        r2 = gaussian_hrf_fitter.get_rsq(gaussian_hrf_pars)
+        pred = gaussian_hrf_fitter.predictions
+
+        final_pars = gaussian_hrf_pars.copy()
+
+    elif model_label == 4:
+        dog_hrf_model = DifferenceOfGaussiansPRF2DWithHRF(data=data, paradigm=paradigm, hrf_model=hrf_model,
+                                                grid_coordinates=grid_coordinates, flexible_hrf_parameters=True)
+        
+        dog_hrf_init_pars = gd_pars.copy()
+
+        dog_hrf_init_pars['srf_amplitude'] = grid_pars['srf_amplitude'] = 1e-3
+        dog_hrf_init_pars['srf_size'] = grid_pars['srf_size'] = 2.
+
+        dog_hrf_init_pars['hrf_delay'] = 4.5
+        dog_hrf_init_pars['hrf_dispersion'] = .75
+
+        dog_hrf_fitter = ParameterFitter(model=dog_hrf_model, data=data, paradigm=paradigm)
+
+        dog_hrf_pars = dog_hrf_fitter.fit(init_pars=dog_hrf_init_pars, max_n_iterations=max_n_iterations)
+
+        r2 = dog_hrf_fitter.get_rsq(dog_hrf_pars)
+
+        pred = dog_hrf_fitter.predictions
+
+        final_pars = dog_hrf_pars.copy()
+
+    else:
+        raise ValueError(f'Unknown model label: {model_label}')
+
+
+    final_pars['theta'] = np.arctan2(final_pars['y'], final_pars['x'])
+    final_pars['ecc'] = np.sqrt(final_pars['x']**2 + final_pars['y']**2)
+    final_pars['r2'] = r2
+
+    for par in final_pars.columns:
+        final_par_img = r2_masker.inverse_transform(final_pars[par])
+        
+        if par in grid_pars.columns:
             grid_par_img = brain_masker.inverse_transform(grid_pars[par])
-            r2_grid_img = brain_masker.inverse_transform(r2_grid)
+        else:
+            grid_par_img = image.math_img('np.zeros_like(img)', img=final_par_img)
 
-            par_img = image.math_img(f'np.where(r2 > {grid_r2_thr}, gd_pars, grid_pars)', 
-                                    r2=r2_grid_img, gd_pars=pars_dog_img, grid_pars=grid_par_img)
+        r2_grid_img = brain_masker.inverse_transform(r2_grid)
 
-            par_img.to_filename(target_dir / f'sub-{subject:02d}_desc-{par}.nii.gz')
+        par_img = image.math_img(f'np.where(r2 > {grid_r2_thr}, final_pars, grid_pars)', 
+                                r2=r2_grid_img, final_pars=final_par_img, grid_pars=grid_par_img)
 
+        par_img.to_filename(target_dir / f'sub-{subject:02d}_desc-{par}.nii.gz')
 
 
 if __name__ == "__main__":
@@ -121,9 +181,10 @@ if __name__ == "__main__":
     argument_parser.add_argument('subject', type=int, help='Subject ID')
     argument_parser.add_argument('--model', type=int, default=1)
     argument_parser.add_argument('--r2_thr', default=0.04, type=float, help='R2 threshold for grid fit')
+    argument_parser.add_argument('--resolution', default=50, type=int, help='Resolution of PRF stimulus')
     argument_parser.add_argument('--bids_folder', default='/data/ds-retsupp', help='BIDS folder path')
-
+    argument_parser.add_argument('--debug', action='store_true', help='Enable debug mode')  
 
     args = argument_parser.parse_args()
 
-    main(args.subject, model_label=args.model, bids_folder=args.bids_folder, grid_r2_thr=args.r2_thr)
+    main(args.subject, model_label=args.model, bids_folder=args.bids_folder, grid_r2_thr=args.r2_thr, debug=args.debug, resolution=args.resolution)
--- retsupp/visualize/import_freesurfer_subject.py
+++ retsupp/visualize/import_freesurfer_subject.py
@@ -13,33 +13,55 @@ def main(subject, bids_folder):
             pycortex_subject=f'retsupp.sub-{subject}',
             freesurfer_subject_dir=op.join(bids_folder, 'derivatives', 'fmriprep', 'sourcedata', 'freesurfer'))
 
-    t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
-            f'sub-{subject}_desc-preproc_T1w.nii.gz')
+    if int(subject) < 3:
+        t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
+                f'sub-{subject}_desc-preproc_T1w.nii.gz')
 
-    fsnative2t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
-            f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.txt')
+        fsnative2t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
+                f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.txt')
 
-    epi = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'func',
-            f'sub-{subject}_ses-1_task-search_rec-NORDIC_run-1_space-T1w_boldref.nii.gz')
+        epi = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'func',
+                f'sub-{subject}_ses-1_task-search_rec-NORDIC_run-1_space-T1w_boldref.nii.gz')
 
-    fsnative2t1w = Affine.from_filename(fsnative2t1w, fmt='itk',
-            reference=t1w)
+        fsnative2t1w = Affine.from_filename(fsnative2t1w, fmt='itk',
+                reference=t1w)
 
-    fsnative2t1w.to_filename(op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
-            f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.fsl'),
-            fmt='fsl')
+        fsnative2t1w.to_filename(op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'anat',
+                f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.fsl'),
+                fmt='fsl')
 
-    pycortex_transform = Transform.from_fsl(op.join(bids_folder, 'derivatives',
-        'fmriprep',
-        f'sub-{subject}', 'anat',
-            f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.fsl'),
-            epi, t1w)
+        pycortex_transform = Transform.from_fsl(op.join(bids_folder, 'derivatives',
+                'fmriprep',
+                f'sub-{subject}', 'anat',
+                f'sub-{subject}_from-fsnative_to-T1w_mode-image_xfm.fsl'),
+                epi, t1w)
+        
+    else:
+        t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'anat',
+                      f'sub-{subject}_ses-1_desc-preproc_T1w.nii.gz')
+
+        fsnative2t1w = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'anat',
+                                   f'sub-{subject}_ses-1_from-fsnative_to-T1w_mode-image_xfm.txt')
+        
+        epi = op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'func',
+                            f'sub-{subject}_ses-1_task-search_rec-NORDIC_run-1_space-T1w_boldref.nii.gz')
+        
+        fsnative2t1w = Affine.from_filename(fsnative2t1w, fmt='itk',
+                                                reference=t1w)
+        fsnative2t1w.to_filename(op.join(bids_folder, 'derivatives', 'fmriprep', f'sub-{subject}', 'ses-1', 'anat',
+                                                f'sub-{subject}_ses-1_from-fsnative_to-T1w_mode-image_xfm.fsl'),
+                                                fmt='fsl')
+
+        pycortex_transform = Transform.from_fsl(op.join(bids_folder, 'derivatives',
+                'fmriprep',
+                f'sub-{subject}', 'ses-1', 'anat',
+                f'sub-{subject}_ses-1_from-fsnative_to-T1w_mode-image_xfm.fsl'),
+                epi, t1w)
+
 
     pycortex_transform.save(f'retsupp.sub-{subject}', 'epi', xfmtype='coord')
 
     identity_transform = Transform(np.identity(4), epi).save(f'retsupp.sub-{subject}', 'epi.identity')
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
