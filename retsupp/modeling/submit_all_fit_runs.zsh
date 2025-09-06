5ff41d63efa026697982200610fd9c5f63f44970
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-09-06 11:49:55 +0200

    Only model 4, all subejcts up to 14

--- retsupp/modeling/submit_all_fit_runs.zsh
+++ retsupp/modeling/submit_all_fit_runs.zsh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
-subjects=(1 2 3 5 10 11 12 13)
+subjects=({1..14})
 sessions=(1 2)
 runs=(1 2 3 4 5 6)
-models=(1 2 3 4)
+models=(4)
 
 for subject in $subjects; do
   for session in $sessions; do
     for run in $runs; do
       for model in $models; do
-        sbatch /Users/gdehol/git/retsupp/retsupp/modeling/fit_run.sh $subject $session $run $model
+        sbatch $HOME/git/retsupp/retsupp/modeling/fit_run.sh $subject $session $run $model
       done
     done
   done
